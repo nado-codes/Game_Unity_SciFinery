ca91e7de936449a5b6016fa7b41f2223850791b7
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2021-09-27 00:48:36 +1000

    Reactor Sample + Animations
    + [Reactor] Added reactor sample + animations for coils

---
+++ Assets/CoilAnim.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+public class CoilAnim : MonoBehaviour
+{
+    private IEnumerable<MeshRenderer> coils;
+    private int coilIndex = 0;
+    private MeshRenderer currentCoilRenderer;
+    private Color coilBaseColor;
+    
+    private DateTime referenceTime = DateTime.Now;
+    private int animIntervalMS = 250;
+
+    void Start()
+    {
+        coils = GetComponentsInChildren<MeshRenderer>().OrderBy(x => x.transform.position.y).ToList();
+        
+        foreach(MeshRenderer coil in coils)
+            coil.material.SetColor("_EMISSION",coil.material.color);
+
+        currentCoilRenderer = coils.ElementAt(coilIndex);
+        coilBaseColor = currentCoilRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentTime = DateTime.Now;
+
+        if(currentTime.Subtract(referenceTime).Milliseconds > animIntervalMS)
+        {
+            currentCoilRenderer.material.DisableKeyword("_EMISSION");
+            
+            coilIndex = (coilIndex < coils.Count()-1) ? coilIndex + 1 : 0;
+
+            currentCoilRenderer = coils.ElementAt(coilIndex);
+            currentCoilRenderer.material.EnableKeyword("_EMISSION");
+
+            referenceTime = currentTime;
+            Debug.Log("animation has changed");
+        }
+
+        Debug.Log(currentTime.Subtract(referenceTime).Milliseconds+"/"+animIntervalMS);
+        Debug.ClearDeveloperConsole();
+    }
+}
