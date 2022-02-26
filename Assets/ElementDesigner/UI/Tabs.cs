25078a0672c1541c68d53a9bc8df783697206cd7
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2022-02-27 00:28:11 +1100

    UI Updates - Molecules + Products
    Working on tabs to allow creation of Molecules and Products

--- Assets/ElementDesigner/UI/Tabs.cs
+++ Assets/ElementDesigner/UI/Tabs.cs
@@ -2,46 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
+using UnityEngine.Events;
 using System.Linq;
 
 public delegate void TabClick(int tabId);
 public class Tabs : MonoBehaviour
 {
-    private List<Button> tabs = new List<Button>();
+    private List<Tab> tabs = new List<Tab>();
 
     public int selectedTabIndex = 0;
 
-    private const int tabSelectedAlpha = 255, tabNormalAlpha = 123;
-    private ColorBlock tabSelectedColors, tabDeselectedColors;
+    public TabClick OnSelectedTabChanged;
 
-    public TabClick onTabChanged;
 
     // Start is called before the first frame update
     void Start()
     {
-        tabs = GetComponentsInChildren<Button>().ToList();
-        Debug.Log("tab count="+tabs.Count);
+        tabs = GetComponentsInChildren<Tab>().ToList();
 
         var firstTab = tabs.FirstOrDefault();
-        var normalColor = firstTab.colors.normalColor;
-        var selectedColor = firstTab.colors.selectedColor;
-        var newNormalColor = new Color(normalColor.r,normalColor.g,normalColor.b,tabNormalAlpha);
-        var newSelectedColor = new Color(selectedColor.r,selectedColor.g,selectedColor.b,tabSelectedAlpha);
-
-        tabDeselectedColors = firstTab.colors;
-        tabDeselectedColors.normalColor = newNormalColor;
-        tabSelectedColors = firstTab.colors;
-        tabSelectedColors.normalColor = newSelectedColor;
-
-        Debug.Log("tabDeselectedColors="+tabDeselectedColors.normalColor);
-        Debug.Log("tabSelectedColors="+tabSelectedColors.normalColor);
 
         tabs.ForEach((tab) => {
             var currentTabIndex = tabs.IndexOf(tab);
 
-            tab.onClick.AddListener(() => handleTabClicked(currentTabIndex)); 
-            tab.colors = (currentTabIndex == selectedTabIndex) ? tabSelectedColors : tabDeselectedColors;
+            tab.OnClick+= () => handleTabClicked(currentTabIndex);
+
+            if(currentTabIndex == selectedTabIndex) 
+                tab.Select();
+            else
+                tab.Deselect();
         });
     }
 
@@ -49,11 +38,11 @@ public class Tabs : MonoBehaviour
     {
         Debug.Log("selected tab "+index);
         var tabToSelect = tabs.FirstOrDefault(tab => tabs.IndexOf(tab) == index);
-        tabToSelect.colors = tabSelectedColors;
+        tabToSelect.Select();
 
         var otherTabs = tabs.Where((_,i) => i != index).ToList();
-        otherTabs.ForEach(tab => tab.colors = tabDeselectedColors);
+        otherTabs.ForEach(tab => tab.Deselect());
 
-        onTabChanged(selectedTabIndex);
+        OnSelectedTabChanged?.Invoke(index);
     }
 }
