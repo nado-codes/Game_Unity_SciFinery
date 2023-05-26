43c657ae63fb9220956fd50f2b205d90880d233a
Nathan <nlinsley@nadocointeractive.com>
2023-05-26 21:21:32 +1000

    Implemented HD Particles

--- Assets/ElementDesigner/World/Elements/WorldParticle.cs
+++ Assets/ElementDesigner/World/Elements/WorldParticle.cs
@@ -8,50 +8,83 @@ using UnityEngine;
 
 public class WorldParticle : WorldElement
 {
-    private WorldElementMotor motor;
-    private WorldElementMotor Motor
+  private WorldElementMotor motor;
+  private WorldElementMotor Motor
+  {
+    get
     {
-        get
-        {
-            if (motor == null)
-                motor = GetComponent<WorldElementMotor>();
-            if (motor == null)
-                throw new ApplicationException("WorldParticle requires a WorldElementMotor to work properly. Please add one first.");
+      if (motor == null)
+        motor = GetComponent<WorldElementMotor>();
+      if (motor == null)
+        throw new ApplicationException("WorldParticle requires a WorldElementMotor to work properly. Please add one first.");
 
-            return motor;
-        }
+      return motor;
+    }
+  }
+
+  private GameObject _orbNoCharge, _orbCharge, _orbNegCharge;
+
+  protected override void Start()
+  {
+    base.Start();
+    VerifyInitialize();
+  }
+
+  protected override void VerifyInitialize()
+  {
+    _orbNoCharge = BodyTransform.Find("Orb_NoCharge")?.gameObject;
+    Assertions.AssertNotNull(_orbNoCharge, "_orbNoCharge");
+    _orbCharge = BodyTransform.Find("Orb_Charge")?.gameObject;
+    Assertions.AssertNotNull(_orbNoCharge, "_orbCharge");
+    _orbNegCharge = BodyTransform.Find("Orb_NegCharge")?.gameObject;
+    Assertions.AssertNotNull(_orbNoCharge, "_orbNegCharge");
+
+    base.VerifyInitialize();
+  }
+
+  protected override void Update()
+  {
+    var audioSource = GetComponentsInChildren<AudioSource>().FirstOrDefault(a => a.clip.name == "PowerlineNoise");
+    if (Data.Charge < 0 && audioSource != null)
+    {
+      if (!audioSource.isPlaying)
+        audioSource.Play();
+    }
+    base.Update();
+  }
+
+  // TODO: Everything is a WorldElement but not every WorldElement can move or has a trail
+  // WorldElementMotor - For movement
+  // WorldElementTrail - For trails
+
+  public void SetParticleData(Particle particleData)
+  {
+    base.VerifyInitialize();
+    base.SetData(particleData);
+
+    if (particleData.Charge < 0)
+    {
+      _orbCharge.SetActive(false);
+      _orbNoCharge.SetActive(false);
+      _orbNegCharge.SetActive(true);
+    }
+    else if (particleData.Charge > 0)
+    {
+      _orbNoCharge.SetActive(false);
+      _orbNegCharge.SetActive(false);
+      _orbCharge.SetActive(true);
+    }
+    else
+    {
+      _orbNegCharge.SetActive(false);
+      _orbCharge.SetActive(false);
+      _orbNoCharge.SetActive(true);
     }
 
-    protected override void Start()
-    {
-        base.Start();
-        VerifyInitialize();
-    }
+    var pSize = particleData.Size;
+    BodyTransform.localScale = new Vector3(pSize, pSize, pSize);
 
-    protected override void Update()
-    {
-        var audioSource = GetComponentsInChildren<AudioSource>().FirstOrDefault(a => a.clip.name == "PowerlineNoise");
-        if (Data.Charge < 0 && audioSource != null)
-        {
-            if (!audioSource.isPlaying)
-                audioSource.Play();
-        }
-        base.Update();
-    }
-
-    // TODO: Everything is a WorldElement but not every WorldElement can move or has a trail
-    // WorldElementMotor - For movement
-    // WorldElementTrail - For trails
-
-    public void SetParticleData(Particle particleData)
-    {
-        base.VerifyInitialize();
-        base.SetData(particleData);
-
-        var pSize = particleData.Size;
-        BodyTransform.localScale = new Vector3(pSize, pSize, pSize);
-
-        ColorUtility.TryParseHtmlString(particleData.ColorHex, out Color particleColor);
-        SetColor(particleColor);
-    }
+    ColorUtility.TryParseHtmlString(particleData.ColorHex, out Color particleColor);
+    SetColor(particleColor);
+  }
 }
